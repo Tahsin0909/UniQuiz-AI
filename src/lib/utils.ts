6a2ee6ce5a4b6bc01e688df4c68a7e7177b57c6e
tahsin090 <tajisan789@gmail.com>
2025-10-03 23:51:23 +0600

    updated quize

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,18 +5,3 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function rock(r: string) {
-  return r
-}
-
-export function tru(r: string) {
-  return r
-}
-
-export function trugt(r: string) {
-  return r
-}
-
-export function truggt(r: string) {
-  return r
-}
