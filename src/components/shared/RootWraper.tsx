7bb396bbfb2941bd1e480420b962bf8466adbfd5
tahsin090 <tajisan789@gmail.com>
2025-10-13 23:54:45 +0600

    fixing root wrapper

--- src/components/shared/RootWraper.tsx
+++ src/components/shared/RootWraper.tsx
@@ -33,4 +33,15 @@ const RootWrapper = ({ children }: { children: React.ReactNode }) => {
 };
 
 export default RootWrapper;
+import React from 'react';
+
+const RootWrapper = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className='pt-40 h-full'>
+            {children}
+        </div>
+    );
+};
+
+export default RootWrapper;
 
